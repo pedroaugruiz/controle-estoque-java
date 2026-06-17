92b1eafba98145de626152e9879f353e37de5f80
pedroaugruiz <pedroaugustoruiz9@gmail.com>
2026-06-17 01:03:19 -0300

    Projeto inicial configurado
    Criação do projeto Java e configuração inicial do ambiente.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Classe principal do sistema
+public class Main {
+
+    // Método principal executado quando o programa inicia
+    public static void main(String[] args) {
+
+        // Exibe uma mensagem no console
+        System.out.println("Sistema de Controle de Estoque");
+
+    }
+}
